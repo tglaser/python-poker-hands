18e269e34f73eff133c4bf8a8a243f1313b48896
NStoppelkamp <N2P@dsone.3ds.com>
2021-04-15 16:55:39 +0200

    mob next [ci-skip] [ci skip] [skip ci]

--- newsph/nsph.py
+++ newsph/nsph.py
@@ -1,8 +1,14 @@
+from tests.sph_test import SimplePokerHands
 from thirdparty.dealerservice import DealerService
 
 
 class NewFancySimplePokerHands:
 
+    dealerService: DealerService
+
+    def __init__(self, dealerService = DealerService() ):
+        self.dealerService = dealerService
+
     def getBestCardFromDealedHand(self):
-        hand = DealerService().dealRandomHand()
-        return hand[-1]
+        hand = self.dealerService.dealRandomHand()
+        return SimplePokerHands().bestFromHand(hand)
--- tests/nsph_test.py
+++ tests/nsph_test.py
@@ -1,10 +1,20 @@
 import unittest
 
 from newsph.nsph import NewFancySimplePokerHands
+from thirdparty.dealerservice import DealerService
+
 
 
 class NewSimplePokerHandsTest(unittest.TestCase):
 
-    def test_initial_fail(self):
-        sut = NewFancySimplePokerHands()
-        self.fail("This is the inital test")
+
+    def test_shouldReturnGivenCard_whenOnlyOneCardInHand(self):
+        class DealerServiceForTest:
+            def dealRandomHand(self):
+                return ["H3"]
+
+        sut = NewFancySimplePokerHands( DealerServiceForTest() )
+
+        bestCard = sut.getBestCardFromDealedHand()
+
+        self.assertEquals("H3", bestCard)
