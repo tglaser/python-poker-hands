b2537405c62c1c7b4ff811129d355ee62a1eb25c
sgn7 <SGN7@dsone.3ds.com>
2021-04-15 15:23:27 +0200

    mob next [ci-skip] [ci skip] [skip ci]

--- tests/sph_test.py
+++ tests/sph_test.py
@@ -3,7 +3,7 @@ import unittest
 
 class SimplePokerHands:
     def bestFromHand(self, param):
-        return param[0]
+        return sorted(param)[0]
 
 
 class SimplePokerHandsTest(unittest.TestCase):
@@ -14,3 +14,18 @@ class SimplePokerHandsTest(unittest.TestCase):
         bestCard = sut.bestFromHand(["H3"])
 
         self.assertEquals("H3", bestCard)
+
+    def test_shouldReturnHighest_whenGivenTwoCards(self):
+        sut = SimplePokerHands()
+
+        bestCard = sut.bestFromHand(["K6", "C10"])
+
+        self.assertEquals("C10", bestCard)
+
+    def test_shouldReturnHighest_whenGivenTwoCardsInvertedOrder(self):
+        sut = SimplePokerHands()
+
+        bestCard = sut.bestFromHand(["C10", "K6"])
+
+        self.assertEquals("C10", bestCard)
+
