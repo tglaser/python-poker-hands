f1e1a3ee62fe35169f3d85500d78dd0bf7e77ad7
MarcelUsesGit <K3B@dsone.3ds.com>
2021-04-15 16:13:54 +0200

    mob next [ci-skip] [ci skip] [skip ci]

--- tests/sph_test.py
+++ tests/sph_test.py
@@ -1,12 +1,21 @@
 import unittest
 
+from typing import Dict
 
 class SimplePokerHands:
+
+    pictures : Dict[str,int] = { "B" : 11,
+                                 "D" : 12,
+                                 "K" : 13,
+                                 "A" : 14 }
+
     def bestFromHand(self, handCards):
-        return sorted(handCards, key=self.readNumericValue)[0]
+        return sorted(handCards, key=self.readNumericValue)[-1]
 
     def readNumericValue(self, card):
-        return card[1:]
+        if card[-1] in self.pictures:
+            return self.pictures.get(card[-1])
+        return int(card[1:])
 
 
 class SimplePokerHandsTest(unittest.TestCase):
@@ -39,3 +48,30 @@ class SimplePokerHandsTest(unittest.TestCase):
 
         self.assertEquals("K10", bestCard)
 
+    def test_shouldReturnK10_whenGivenFiveCards(self):
+        sut = SimplePokerHands()
+
+        bestCard = sut.bestFromHand(["C6", "K10", "C2", "C8", "H7"])
+
+        self.assertEquals("K10", bestCard)
+
+    def test_shouldReturnHB_whenGivenFiveCardsWithJacks(self):
+        sut = SimplePokerHands()
+
+        bestCard = sut.bestFromHand(["C6", "K10", "C2", "HB", "H7"])
+
+        self.assertEquals("HB", bestCard)
+
+    def test_shouldReturnPD_whenGivenFiveCardsWithPictures(self):
+        sut = SimplePokerHands()
+
+        bestCard = sut.bestFromHand(["C6", "K10", "PD", "HB", "H7"])
+
+        self.assertEquals("PD", bestCard)
+
+    def test_shouldReturnKA_whenGivenFiveCardsWithPictures(self):
+        sut = SimplePokerHands()
+
+        bestCard = sut.bestFromHand(["C6", "K10", "PD", "KK", "KA"])
+
+        self.assertEquals("KA", bestCard)
